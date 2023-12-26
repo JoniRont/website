b5cab80ae300dc78df8f5581f2af0cd72fc694e8
Joni Röntynen <57128169+JoniRont@users.noreply.github.com>
2023-12-26 17:34:16 +0200

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    // throw new Error('Method not implemented.');
-  }
+export class AppComponent  {
+
   title = 'website';
   scrollToIntroduction(id: string){
     let el = document.getElementById(id);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { SamplesComponent } from './components/samples/samples.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { MatIconModule } from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
@@ -26,6 +26,7 @@ import { MainContentComponent } from './components/main-content/main-content.com
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
